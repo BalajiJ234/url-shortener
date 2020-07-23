6a29798f7c475db6b9a0ca251e3e8843d7803a71
Balaji Janarthanan <31264281+BalajiJ234@users.noreply.github.com>
2020-07-23 11:01:31 +0530

    Adding Passport-Jwt authentication, routes for login, register, import appropriate fields in server.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb+srv://balaji-dev:1242523Jdbr@urlshortner.cjii8.mongodb.net/urlshortener?retryWrites=true&w=majority" 
+  mongoURI: "mongodb+srv://balaji-dev:1242523Jdbr@urlshortner.cjii8.mongodb.net/urlshortener?retryWrites=true&w=majority",
+  secretOrKey: "secret"
 }
