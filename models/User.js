a1653f24b4fe8f4af77343727018eae3f77e9e36
Balaji Janarthanan <31264281+BalajiJ234@users.noreply.github.com>
2020-07-23 10:09:54 +0530

    User Models, Validation for Login, register
    Creating user Model, Validation for register and login.

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+//Create Schema
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: true
+  },
+  password: {
+    type: String,
+  },
+  urls_created: {
+    type: Array,
+    default: []
+  }
+},
+{ timestamps: true}
+)
+
+module.exports = mongoose.model("User", userSchema)
